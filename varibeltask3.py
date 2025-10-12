3556fe06f89b703f6095a2e70b8e473b99b05aae
Arjun Goel <adityaagoel@gmail.com>
2025-10-12 11:18:00 -0700

    Add files via upload

---
+++ varibeltask3.py
@@ -0,0 +1,12 @@
+"""TASK 3 - Write a Python program using the input function to:
+
+Ask the user to enter their account number.
+Ask the user to enter their PIN code.
+Ask the user to enter the amount they want to withdraw.
+Finally, print a message showing all the details entered by the user."""
+accountNumber=input("what is your account Number ")
+pinCode=input("what is your pincode ")
+withDrawamount=input("what is your with draw amount ")
+print("Account Number: ",accountNumber)
+print("Pin Code: ",pinCode)
+print("Withdraw amount: ",withDrawamount)
